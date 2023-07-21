c85651fb27dda5452b6bf5fc7ac82700e5d72f91
alex <alexangel.j@gmail.com>
2023-07-20 21:54:17 -0700

    update deps

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 forge clean
 
-forge install
+forge install foundry-rs/forge-std --no-commit
+forge install transmissions11/solmate@v7 --no-commit
+forge install https://github.com/primitivefinance/portfolio@3e2ed512790db33088427a4611f69f4849ede95b --no-commit
 
 forge bind --crate-name bindings --overwrite
 
