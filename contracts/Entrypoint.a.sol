8149c5cd024309e99b6455ae2b492304ba9916b8
alex <alexangel.j@gmail.com>
2023-07-20 21:50:55 -0700

    contract work

--- contracts/Entrypoint.a.sol
+++ contracts/Entrypoint.a.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import "solmate/tokens/ERC20.sol";
+import "solmate/test/utils/mocks/MockERC20.sol";
+
 import "./ArbiterContract.sol";
 import { Actor } from "./Actor.sol";
 
 contract Entrypoint is ArbiterContract {
-    address public actor;
+    uint256 public constant startBalance = 4_809e18;
+
+    address public _actor;
+    address public _subject;
     address public token0;
     address public token1;
 
@@ -15,6 +21,31 @@ contract Entrypoint is ArbiterContract {
         override
         returns (bytes memory output)
     {
-        actor = address(new Actor());
+        _actor = address(new Actor());
+        token0 = address(new MockERC20("Mock0", "X", 18));
+        token1 = address(new MockERC20("Mock1", "Y", 18));
+
+        // token approvals
+        Actor(_actor).execute(
+            token0,
+            abi.encodeWithSignature(
+                ERC20.approve.selector, address(this), type(uint256).max
+            )
+        );
+
+        Actor(_actor).execute(
+            token1,
+            abi.encodeWithSignature(
+                ERC20.approve.selector, address(this), type(uint256).max
+            )
+        );
+
+        // token minting
+        ERC20(token0).mint(_actor, startBalance);
+        ERC20(token1).mint(_actor, startBalance);
+    }
+
+    function actor() public view returns (address) {
+        return _actor;
     }
 }
