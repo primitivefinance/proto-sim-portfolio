90db758ce19512ce64149c52f7fef3c6089546bd
alex <alexangel.j@gmail.com>
2023-07-20 20:52:48 -0700

    we are cookin

---
+++ contracts/Exchange.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+interface IERC20 {
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function decimals() external view returns (uint256);
+}
+
+/// @dev Simple infinitely liquid exchange!
+contract Exchange {
+    mapping(address denomination => mapping(address token => uint256 price))
+        public _prices;
+
+    function getPrice(
+        address token,
+        address denomination
+    ) public view returns (uint256) {
+        return _prices[denomination][token]; // price = token / denomination
+    }
+
+    /// @dev Sets a price for a token in a denomination, in WAD units.
+    function set(address token, address denomination, uint256 price) public {
+        _prices[denomination][token] = price;
+    }
+
+    function trade(
+        address asset,
+        address quote,
+        bool sellAsset,
+        uint256 amount
+    ) public {
+        uint256 price =
+            sellAsset ? _prices[quote][asset] : _prices[asset][quote]; // selling asset ? price = quote / asset : price = asset / quote
+
+        uint256 unit = 10 ** IERC20(sellAsset ? quote : asset).decimals();
+
+        _debit(sellAsset ? asset : quote, amount);
+        _credit(sellAsset ? quote : asset, amount * price / unit);
+    }
+
+    function _debit(address token, uint256 amount) internal {
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function _credit(address token, uint256 amount) internal {
+        IERC20(token).transfer(msg.sender, amount);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
-use arbiter::manager;
+use arbiter::{manager, environment::contract::SimulationContract};
+use bindings::{exchange};
+use arbiter::agent::Agent;
+use m3_rs::models::{base_model::BaseModel, rmm_01::RMM01};
 
 
 fn main() {
     let mut manager = manager::SimulationManager::new();
+    println!("Hello, world! Simulation is running.");
 
-    print!("Hello, world! Simulation is running.");
+    let admin = manager.agents.get("admin").unwrap();
+    let exchange = SimulationContract::new(exchange::EXCHANGE_ABI.clone(), exchange::EXCHANGE_BYTECODE.clone());
+    let(exchange, result) = admin.deploy(exchange, Vec::new()).unwrap();
+    manager.deployed_contracts.insert("exchange".to_string(), exchange);
+    
+    assert!(result.is_success());
+    println!("Gas used: {:?}", result.gas_used());
+    println!("Exchange address: {:?}", manager.deployed_contracts.get("exchange").unwrap().address);
+    println!("Success!");
+
+
+    let mut strategy = BaseModel::new(
+        "NormalStrategy".to_string(),
+        "v1.4.0-beta".to_string(),
+        "x".to_string(),
+        "id".to_string()
+    );
+
+    strategy.set_objective(Box::new(RMM01{
+        strike: 1_f64,
+        volatility: 0.1_f64,
+        time_to_maturity: 1.0_f64
+    }));
+
+    let price = strategy.objective.expect("No objective set!").get_reported_price();
+    println!("Price: {:?}", price);
 }
