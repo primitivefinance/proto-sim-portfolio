b898c3f08aecc32cd9cd7a14b7ddfb36d84d2036
alex <alexangel.j@gmail.com>
2023-07-22 15:53:29 -0700

    fix(portfolio-import): adds contract that imports portfolio

---
+++ contracts/HackDependencyImports.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// For the .sol artifacts to be generated from `forge build` and thus have bindings generated
+// with `forge bind`, we have to have the contracts imported into at least one .sol file
+// in this local repo's contracts directory.
+import "solmate/tokens/WETH.sol";
+import "portfolio/Portfolio.sol";
+
+contract IgnoreContract { }
